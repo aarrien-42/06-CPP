59541cd430d1b81e2c10e75d3a395bcccb56ee6e
Andoni Arrien Martin <aarrien-@c2r8s2.42urduliz.com>
2023-06-13 14:19:56 +0200

    reference identify function done

--- Module_06/ex02/main.cpp
+++ Module_06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aarrien- <aarrien-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 15:53:36 by aarrien-          #+#    #+#             */
-/*   Updated: 2023/06/01 15:13:11 by aarrien-         ###   ########.fr       */
+/*   Updated: 2023/06/13 14:18:51 by aarrien-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,11 +44,38 @@ void identify(Base *p) {
 	}
 }
 
+void identify(Base &p) {
+	try {
+		A& ARef = dynamic_cast<A&>(p);
+		if (&ARef != nullptr) {
+			std::cout << "A\n";
+		}
+	} catch (...) {
+		try {
+			B& BRef = dynamic_cast<B&>(p);
+			if (&BRef != nullptr) {
+				std::cout << "B\n";
+			}
+		} catch (...) {
+			try {
+				C& CRef = dynamic_cast<C&>(p);
+				if (&CRef != nullptr) {
+					std::cout << "C\n";
+				}
+			} catch (...) {
+				std::cout << "Cannot identify this class!\n";
+			}
+		}
+	}
+}
+
 int main() {
 	Base* B;
 	B = generate();
-	std::cout << "identified: ";
+	std::cout << "  identified (pointer identify):   ";
 	identify(B);
+	std::cout << "  identified (reference identify): ";
+	identify(*B);
 	delete B;
 	return 0;
 }
