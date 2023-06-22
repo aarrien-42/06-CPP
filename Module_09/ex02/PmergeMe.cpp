4ef53f48b9cda4a2ff4662f93536a90accc7a861
Andoni Arrien Martin <aarrien-@c3r5s5.42urduliz.com>
2023-06-22 15:06:02 +0200

    merge-insert-sort implemented for a vector

--- Module_09/ex02/PmergeMe.cpp
+++ Module_09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: aarrien- <aarrien-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 13:50:44 by aarrien-          #+#    #+#             */
-/*   Updated: 2023/06/20 16:43:58 by aarrien-         ###   ########.fr       */
+/*   Updated: 2023/06/22 15:01:49 by aarrien-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,39 +32,84 @@ PmergeMe& PmergeMe::operator=( const PmergeMe& obj ) {
 }
 
 std::ostream& operator<<( std::ostream& os, const PmergeMe& obj ) {
-	obj.showVector();
+	std::vector<int> sequence = obj.getVector();
+	for (std::vector<int>::const_iterator it = sequence.cbegin(); it < sequence.cend(); it++) {
+		os << *it << " ";
+	}
 	return os;
 }
 
 /*-MÉTODOS-*/
 
-std::vector<Pair> PmergeMe::getVector() const { return _vSequence; }
+std::vector<int> PmergeMe::getVector() const { return _vSequence; }
 
-void PmergeMe::addPair( const Pair& p ) {
-	_vSequence.push_back(p);
+void PmergeMe::vAddNum( int num ) {
+	_vSequence.push_back(num);
 }
 
-void PmergeMe::showVector() const {
-	for (std::vector<Pair>::const_iterator it = _vSequence.cbegin(); it < _vSequence.cend(); it++) {
-		std::cout << "(" << it->nums[0] << (it->pair ? " " + std::to_string(it->nums[1]) : "") << ") ";
-	}
-	std::cout << "\n";
+std::vector<int>::iterator PmergeMe::insertBinarySearch(int num, std::vector<int>::iterator first, std::vector<int>::iterator last) {
+	if (num <= *first)
+		return first;
+	if (*first < num && *last > num && last - first == 1)
+		return first + 1;
+	if (*(first+(floor((last-first)/2))) > num)
+		first = insertBinarySearch(num, first, first+(floor((last-first)/2)));
+	else
+		first = insertBinarySearch(num, first+(floor((last-first)/2)), last);
+	return first;
 }
 
 void PmergeMe::sortVector() {
-	for (std::vector<Pair>::iterator it1 = _vSequence.begin(); it1 < _vSequence.end() - 1; it1++) {
-		for (std::vector<Pair>::iterator it2 = it1 + 1; it2 < _vSequence.end(); it2++)
-		if (it1->pair && it2->pair && it1->nums[0] > it2->nums[0])
-			std::swap(it1->nums, it2->nums);
+	//std::cout << "input:      " << *this << "\n"; // PRINT
+	for (std::vector<int>::iterator it = _vSequence.begin(); it < _vSequence.end() - 1; it+=2) {
+		if (*it < *(it+1))
+			std::swap(*it, *(it+1));
 	}
-	for (std::vector<Pair>::iterator it1 = _vSequence.begin(); it1 < _vSequence.end() - 1; it1++) {
-		int num = it1->nums[1];
-		it1->nums[1] = -1;
-		for (std::vector<Pair>::iterator it2 = it1; it2 >= _vSequence.begin(); it2--) {
-			if (it2 == _vSequence.begin() || it2->nums[0] > num) {
-				_vSequence.insert(it2 - 1, Pair(num, -1));
-				break;
+	//std::cout << "sort pairs: " << *this << "\n"; // PRINT
+
+	for (std::vector<int>::iterator it1 = _vSequence.begin(); it1 < _vSequence.end() - 3; it1+=2) {
+		for (std::vector<int>::iterator it2 = it1 + 2; it2 < _vSequence.end() - 1; it2+=2) {
+			if (*it1 > *(it2)) {
+				std::swap(*it1, *it2);
+				std::swap(*(it1+1), *(it2+1));
 			}
 		}
 	}
+	//std::cout << "sort main:  " << *this << "\n"; // PRINT
+
+	std::vector<int> b;
+	for (std::vector<int>::iterator it = _vSequence.begin(); it < _vSequence.end(); it+=2) {
+		((it+1) < _vSequence.end()) ? b.push_back(*(it+1)) : b.push_back(*it);
+	}
+	if (_vSequence.size() % 2)
+		_vSequence.erase(_vSequence.end() - 1);
+	for (std::vector<int>::iterator it = _vSequence.begin() + 1; it < _vSequence.end(); it++) {
+		_vSequence.erase(it);
+	}
+	// PRINT
+	//std::cout << "main   :    " << *this << "\n";
+	//std::cout << "divided:    ";
+	//for (std::vector<int>::iterator it = b.begin(); it < b.end(); it++) {
+	//	std::cout << *it << " ";
+	//}
+	//std::cout << "\n";
+	//END PRINT
+	for (std::vector<int>::iterator it = b.begin(); it < b.end(); it++) {
+		if (it == b.begin())
+			_vSequence.insert(_vSequence.begin(), *it);
+		else
+			_vSequence.insert(insertBinarySearch(*it, _vSequence.begin(), _vSequence.end() - 1), *it);
+	}
+	// PRINT
+	std::cout << "main   :    " << *this << "\n";
+	std::vector<int>::iterator it;
+	for (it = _vSequence.begin(); it < _vSequence.end()-1; it++) {
+		if (*it > *(it+1)) {
+			std::cout << "FAILED: " << *it << " > " << *(it+1) << "\n";
+			break;
+		}
+	}
+	if (it == _vSequence.end()-1)
+		std::cout << "SUCCESS\n";
+	//END PRINT
 }
--- Module_09/ex02/PmergeMe.hpp
+++ Module_09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: aarrien- <aarrien-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 13:50:57 by aarrien-          #+#    #+#             */
-/*   Updated: 2023/06/20 16:20:43 by aarrien-         ###   ########.fr       */
+/*   Updated: 2023/06/22 14:38:24 by aarrien-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,9 @@
 #include <iostream>
 #include <vector>
 
-struct Pair {
-	Pair(int num1, int num2) {
-		nums[0] = num1;
-		nums[1] = num2;
-		if (num2!= -1 && num1 > num2)
-			std::swap(nums[0], nums[1]);
-		pair = num2 != -1;
-	}
-	int nums[2];
-	bool pair;
-};
-
 class PmergeMe {
 	private:
-		std::vector<Pair> _vSequence;
+		std::vector<int> _vSequence;
 	public:
 		PmergeMe();
 		PmergeMe( const PmergeMe& obj );
@@ -38,10 +26,10 @@ class PmergeMe {
 
 		PmergeMe& operator=( const PmergeMe& obj );
 
-		std::vector<Pair> getVector() const;
-		void addPair( const Pair& p );
-		void showVector() const;
+		std::vector<int> getVector() const;
+		void vAddNum( int num );
 
+		std::vector<int>::iterator insertBinarySearch(int num, std::vector<int>::iterator first, std::vector<int>::iterator last);
 		void sortVector();
 };
 
